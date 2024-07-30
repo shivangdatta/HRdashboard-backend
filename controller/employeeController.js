8cc71e196afcbb662ecaa1d2eef99c52b638e18b
shivang datta <shivangdatta@gmail.com>
2024-07-31 02:59:11 +0530

    updates

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,16 +1,49 @@
-const asyncHandler = require('express-async-handler')
-const Employee = require('../models/employee')
+const asyncHandler = require('express-async-handler');
+const Employee = require('../models/employee');
 
+const getAllEmployees = asyncHandler(async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
-const getAllEmployees = asyncHandler(async (req , res) => {
-    const records = await Employee.find()
+  try {
+    const records = await Employee.aggregate([
+      {
+        "$project": {
+          "_id": 1,
+          "employeeId": 1,
+          "name": 1,
+          "age": 1
+        }
+      },
+      { "$skip": skip },
+      { "$limit": limit }
+    ]);
 
-    try{
-        res.status(200).json(records)
+    res.status(200).json(records);
+  } catch (err) {
+    res.status(501).json({ msg: `Failed to retrieve data. Error: ${err}` });
+  }
+});
+
+const getEmployeebyId = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const employee = await Employee.findById(id)
+
+    if (employee) {
+      res.status(200).json(employee);
+    } else {
+      res.status(404).json({ msg: 'Employee not found' });
     }
-    catch(err){
-        res.status(501).json({msg : `Failed to retrieve data error : ${err}` })
-    }
-})
+  } catch (err) {
+    res.status(500).json({ msg: `Failed to retrieve data. Error: ${err}` });
+  }
+});
 
-module.exports = {getAllEmployees}
+// const getEmployeebyName = asyncHandler(async(req , res) => {
+//   const { name } = req.params;
+// })
+
+module.exports = { getAllEmployees, getEmployeebyId };
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const employeeController = require('../controller/employeeController')
-const router = express.Router()
+const express = require('express');
+const employeeController = require('../controller/employeeController');
+const router = express.Router();
 
 router.route('/')
-    .get(employeeController.getAllEmployees)
+    .get(employeeController.getAllEmployees);
 
+router.route('/:id')
+    .get(employeeController.getEmployeebyId);
 
-module.exports = router
-
+module.exports = router;
